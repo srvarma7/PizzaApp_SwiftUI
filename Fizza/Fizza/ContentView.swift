e72c37763ff88ebd7a18d4a1ba08486c7e093805
srvarma7 <srvarma7@gmail.com>
2020-09-04 17:49:51 +1000

    Adding project files

---
+++ Fizza/Fizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fizza
+//
+//  Created by Sai Raghu Varma Kallepalli on 4/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
