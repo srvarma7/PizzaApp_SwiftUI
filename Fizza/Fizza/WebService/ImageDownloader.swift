4ecf27eeb863a1de9783b1fc03e0f9091ccf4d50
srvarma7 <srvarma7@gmail.com>
2020-09-06 11:36:47 +1000

    Implementing helper to download image from web

---
+++ Fizza/Fizza/WebService/ImageDownloader.swift
@@ -0,0 +1,43 @@
+//
+//  ImageDownloader.swift
+//  Fizza
+//
+//  Created by Sai Raghu Varma Kallepalli on 6/9/20.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class ImageDownloader: ObservableObject {
+    
+    @Published var downloadedImageData: Data?
+    
+    init(imageData: Data) {
+        self.downloadedImageData = imageData
+    }
+    
+    func getImage(link: String) {
+        convertLinkToURL(link: link)
+    }
+    
+    private func convertLinkToURL(link: String) {
+        guard let urlLink = URL(string: link) else {
+            print("Error while converting link to URL")
+            return
+        }
+        downloadDatabyLink(urlLink: urlLink)
+    }
+    
+    private func downloadDatabyLink(urlLink: URL) {
+        URLSession.shared.dataTask(with: urlLink) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Error while Downloading image data")
+                return
+            }
+            DispatchQueue.main.async {
+                self.downloadedImageData = data
+            }
+        }.resume()
+    }
+}
